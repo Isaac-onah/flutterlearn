6d67944a75c36c9c7ff11fe99ade0ef42c59b7e6
Isaac Onah <iumeha@yahoo.com>
2022-01-25 17:05:37 +0100

    Splash Screen

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 
 class MyHomePage extends StatefulWidget {
@@ -15,25 +16,52 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Expanded(child: Image.asset('asset/flutter.png')),
-                Text('with Aiyzihk')
+                Row(
+                  children: <Widget>[
+                    Expanded(child: Image.asset('asset/flutter.png')),
+                    Text('with Aiyzihk')
+                  ],
+                ),
+                Image.asset('asset/Capturee.png'),
+                Text(
+                  'Welcome! I am delighted to learn Flutter with you', style: GoogleFonts.poppins(fontSize: 21, fontWeight:FontWeight.w600),textAlign: TextAlign.center,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20.0),
+                  child: Text(
+                    'I made available lots of flutter UI templates that you can seamlessly use to start and complete your flutter application project with little or no stress. I hope this will be helpfull.',
+                    style: GoogleFonts.poppins(
+                      fontSize: 18,
+                    ), textAlign: TextAlign.center,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(1.0),
+                  child: Container(
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    child: FlatButton(
+                        onPressed: () {
+                        },
+                        child: Text(
+                          "Let's Flutter",
+                          style: TextStyle(color: Colors.white),
+                        )),
+                  ),
+                )
               ],
             ),
-            Image.asset('asset/Capturee.png'),
-            Text(
-              'Welcome! I am delighted to learn Flutter with you'
-            ),
-            Text(
-              'I made available lots of flutter UI templates that you can seamlessly use to start and complete your flutter application project with little or no stress. I hope this will be helpfull.',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
